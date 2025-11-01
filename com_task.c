09214f8080fc6feca4830d224cd5a313e40eba8e
Alejo <jortizmez@gmail.com>
2025-11-01 17:07:37 +0100

    Simply set up for tasks. Several function propotype descriptions are missing. No task has task queue length yet (it's 0). The COM task implements the original implementation given by the prof

--- com_task.c
+++ com_task.c
@@ -15,6 +15,7 @@
 *********************************************************************************************************
 */
 #include <com_task.h>
+#include <includes.h>
 
 
 /*
@@ -22,8 +23,8 @@
 *                                             LOCAL DEFINES
 *********************************************************************************************************
 */
-
-
+static OS_TCB App_TaskCom_TCB;
+static CPU_STK App_TaskComStk_R[APP_CFG_TASK_COM_STK_SIZE];
 /*
 *********************************************************************************************************
 *                                            LOCAL VARIABLES
@@ -36,5 +37,142 @@
 *                                         FUNCTION PROTOTYPES
 *********************************************************************************************************
 */
+
+/*
+*********************************************************************************************************
+*                                          App_TaskCom()
+*
+* Description : COM Task checks for available bytes within the UART receive buffer. If correct string is
+*               available (e.g. PC -> uC: #abc$ or #Hellor World$), process the message and output a
+*               pre-defined string via UART and append the user-defined string via the UART interface.
+*
+* Argument(s) : p_arg   is the argument passed to 'App_TaskCom()' by 'OSTaskCreate()'.
+*
+* Return(s)   : none
+*
+* Note(s)     : none
+*********************************************************************************************************
+*/
+
+void  App_TaskCom (void *p_arg)
+{
+  /* declare and define task local variables */
+  OS_ERR       os_err;
+  CPU_INT08U   rec_byte = 0x00;
+  CPU_INT08U   rx_msg[UART_1_RX_BUFFER_SIZE] = {0};
+  CPU_INT08U   idx = 0x00;
+  CPU_INT08U   rec_byte_cnt = 0x00;
+  CPU_BOOLEAN  str_available = DEF_FALSE;
+  
+  /* prevent compiler warnings */
+  (void)p_arg;
+  (void)Start_of_Packet;
+  (void)End_of_Packet;
+  
+  /* start of the endless loop */
+  while (DEF_TRUE) {
+    /* check if a byte is available */
+    rec_byte = uart_get_byte();
+    /* check if the received byte is '#'*/
+    if(rec_byte == Start_of_Packet){
+      /* if received byte was correct */
+      while(DEF_TRUE){
+        /* receive byte by byte */
+        rec_byte = uart_get_byte();
+        /* check is byte is something meaningful */
+        if(rec_byte){
+          /* save byte into software receive buffer and increment idx */
+          rx_msg[idx++] = rec_byte;
+        }
+        /* initiate scheduler */
+        OSTimeDlyHMSM(0, 0, 0, 20, 
+                      OS_OPT_TIME_HMSM_STRICT, 
+                      &os_err);
+        /* check if received byte is '$' */
+        if(rx_msg[idx-1]==End_of_Packet){
+          /* if end of packet is reached -> break */
+          break;
+        }
+      }
+      /* message received, calculate received bytes, -2 because of '#' & '$' */
+      rec_byte_cnt = idx-2;
+      /* signal that a string is available */
+      str_available = DEF_TRUE;
+    }
+    /* if received byte wasn't start of packet */
+    else{
+      /* reset software receive buffer */
+      memset(&rx_msg[0],0,sizeof(rx_msg));
+      /* reset string available signal */
+      str_available = DEF_FALSE;
+      /* reset received byte variable */
+      rec_byte = 0x00;
+      /* reset idx */
+      idx = 0x00;
+    }
+    /* check if message is available */
+    if(str_available){
+      /* send pre-defined bytes via UART interface */
+      uart_send_byte('C');
+      uart_send_byte('Y');
+      uart_send_byte('8');
+      uart_send_byte('K');
+      uart_send_byte('I');
+      uart_send_byte('T');
+      uart_send_byte(':');
+      /* send received message without '#' and '$' */
+      for(idx=0;idx<=rec_byte_cnt;idx++){
+        uart_send_byte(rx_msg[idx]);
+      }
+      /* reset software receive buffer */
+      memset(&rx_msg[0],0,sizeof(rx_msg));
+      /* reset string available signal */
+      str_available = DEF_FALSE;
+      /* reset received byte variable */
+      rec_byte = 0x00;
+      /* reset idx */
+      idx = 0x00;
+    }
+    /* initiate scheduler */
+    OSTimeDlyHMSM(0, 0, 0, GENERAL_TASK_DELAY_MS, OS_OPT_TIME_HMSM_STRICT, 
+      &os_err);
+  }
+}
+
+/*
+*********************************************************************************************************
+*                                          GetComTaskTCB()
+*
+* Description : 
+*
+* Argument(s) : none
+*
+* Return(s)   : none
+*
+* Note(s)     : none
+*********************************************************************************************************
+*/
+OS_TCB* GetComTaskTCB()
+{
+  return &App_TaskCom_TCB;
+}
+
+/*
+*********************************************************************************************************
+*                                          GetComTaskStk()
+*
+* Description : 
+*
+* Argument(s) : none
+*
+* Return(s)   : none
+*
+* Note(s)     : none
+*********************************************************************************************************
+*/
+CPU_STK* GetComTaskStk()
+{
+  return &App_TaskComStk_R[0];
+}
 
 /* [] END OF FILE */
--- dshot_task.h
+++ dshot_task.h
@@ -11,8 +11,54 @@
 */
 #ifndef DSHOT_TASK_H
 #define DSHOT_TASK_H
-  
-  
+
+#include <os.h>
+#include <cpu.h>
+
+/*
+*********************************************************************************************************
+*                                          App_TaskDshot()
+*
+* Description : 
+*
+* Argument(s) : p_arg   is the argument passed to 'App_TaskDshot()' by 'OSTaskCreate()'.
+*
+* Return(s)   : none
+*
+* Note(s)     : none
+*********************************************************************************************************
+*/
+void App_TaskDshot(void *p_arg);
+
+/*
+*********************************************************************************************************
+*                                          GetDshotTaskTCB()
+*
+* Description : 
+*
+* Argument(s) : none
+*
+* Return(s)   : none
+*
+* Note(s)     : none
+*********************************************************************************************************
+*/
+OS_TCB* GetDshotTaskTCB();
+
+/*
+*********************************************************************************************************
+*                                          GetDshotTaskStk()
+*
+* Description : 
+*
+* Argument(s) : none
+*
+* Return(s)   : none
+*
+* Note(s)     : none
+*********************************************************************************************************
+*/
+CPU_STK* GetDshotTaskStk();
 
 #endif
 /* [] END OF FILE */
